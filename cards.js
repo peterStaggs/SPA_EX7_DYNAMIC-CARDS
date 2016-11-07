5396edfd0fd4749dab037632ca8bdc6224d73926
Peter Staggs <peter.staggs@csuglobal.edu>
2016-11-07 14:04:20 -0600

    Fixed issue with delete buttons--cards now individually delete

--- cards.js
+++ cards.js
@@ -1,22 +1,35 @@
-/*When the user enters in text into the text area and then clicks the create button, 
-create a new card element in the DOM. 
-You decide the height/width of the card.
-When the user clicks the Delete button, the containing card, and no other cards, 
-should then be removed from the DOM. Not just made invisible, actually removed from the DOM.*/
+//Define HTML Elements
 let create = document.getElementById("Create"); 
 let output = document.getElementById("output");
-
+//add an event listener to the create button that generates the cards, along with the value of the user input, and outputs them in the 'output' div of the HTML
+//Call a function that will make the delete buttons work, newDel, here. It doesn't matter that you haven't defined it yet.
 create.addEventListener("click", function () {
-	let maths = Math.floor((Math.random() * 60) + 9);
-	output.innerHTML += "<div class='innerDiv' " + "id='innerDiv'" + maths + "'>" + document.getElementById("input").value + "<button id='clear'" + "class='clear'" + maths + "'>" + "Clear" +  "</button>" + '</div>';
-
+	output.innerHTML += "<div class='innerDiv' " + "id='innerDiv'" + "'>" + document.getElementById("input").value + "<button id='clear'" + "class='clear'" + "'>" + "Clear" +  "</button>" + '</div>';
+	newDel();
 }); 
-
-document.addEventListener('click', function (event) {
-	if (event.target === document.getElementById('clear')) {
-	event.target.parentElement.remove(); 
+//Define the delete button's task with a function:
+function newDel () {
+	//1) Create a variable that contains the classname of the delete button (clear). Use class rather than id, because there will be multiple buttons 
+	var clearbtn = document.getElementsByClassName('clear');
+	//2) create a for loop---you will need to iterate through each time a button shows up 
+	for (let i = 0; i < clearbtn.length; i++) {
+		//3) Define newone--which is the button, loaded with a for loop
+		var newone = clearbtn[i];
+		//4) Give newone a click event listener 
+		newone.addEventListener('click', function (event) {
+		//5) Use event.path to specify what exactly in the DOM you want the delete button to remove. In this case, 
+		//its innerDiv, for each individual instance of the button. This gets called up in the anonymous function of the 
+		//first event listener.
+		event.path[1].remove(); 
+	});
+	} 
 }
-});
+
+
+
+
+
+
 
 
 
